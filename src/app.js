0c92b893fec78966e0fb23bda01da6f8c766fdcd
leonarduslitik <leonarduslitik@gmail.com>
2019-02-10 01:22:20 -0500

    base rnw

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  TouchableHighlight,
+  Animated,
+  Easing,
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+        <Text>Halo dunia...</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
